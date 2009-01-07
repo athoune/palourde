4ff0b19ac0f85ffad13ab089488a85bf85807ce8
mathieu <mathieu@905b5be4-2df4-43e5-8d81-de9f273a5a26>
2009-01-07 22:05:42 +0000

    rangement massif
    git-svn-id: https://admin.garambrogne.net/subversion/palourde/trunk/agent/Palourde@9 905b5be4-2df4-43e5-8d81-de9f273a5a26

---
+++ main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  Palourde
+//
+//  Created by Mathieu on 07/01/09.
+//  Copyright garambrogne.net 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
