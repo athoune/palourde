4685e218766cd030ce0e0ff5b874c46062faa3a7
Mathieu Lecarme <mathieu@garambrogne.net>
2009-04-28 18:59:57 +0200

    action panel

--- source/ActionController.h
+++ source/ActionController.h
@@ -8,9 +8,16 @@
 
 #import <Cocoa/Cocoa.h>
 
+#define NOTHING 1
+#define GROWING 2
+#define FULL 3
 @interface ActionController : NSWindowController {
     IBOutlet NSOutlineView *actionTable;
     IBOutlet NSProgressIndicator *thermometre;
+    int state;
+    int value;
+    double max;
 }
 
+-(void) reset;
 @end
--- source/ActionController.m
+++ source/ActionController.m
@@ -21,5 +21,38 @@ NSArray*    _classNames;
 }
 
 -(void) awakeFromNib {
+    [thermometre setBezeled:false];
+    NSDistributedNotificationCenter *distributedCenter = [NSDistributedNotificationCenter defaultCenter];
+    [distributedCenter addObserver:self
+			  selector:@selector(freshclamDownload:)
+			      name:@"net.clamav.freshclam.download"
+			    object:nil];
+    [self reset];
 }
+
+-(void) reset {
+    state = NOTHING;
+    value = 0;
+    [thermometre setDoubleValue:0];
+}
+
+- (void) freshclamDownload: (NSNotification *)notification {
+    NSLog(@"Thermo FreshClam Download %@", [notification userInfo] );
+    if( state == NOTHING) {
+	state = GROWING;
+	[thermometre setIndeterminate:false];
+	max = [[[notification userInfo] objectForKey:@"total" ] doubleValue];
+	[thermometre displayIfNeeded];
+    }
+    int percent = (int) (100 * [[[notification userInfo] objectForKey:@"downloaded"] doubleValue] / max);
+    NSLog(@"percent : %i", value);
+    if(percent > value) {
+	value = percent;
+	[thermometre setDoubleValue: (double)value];
+    }
+    if([[[notification userInfo] objectForKey:@"total" ] doubleValue] == [[[notification userInfo] objectForKey:@"downloaded"] doubleValue]) {
+	[self reset];
+    }
+}
+
 @end
